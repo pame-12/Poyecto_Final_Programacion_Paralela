ef8de72b9e69fba0d4f284e65a6c3ac2424144ac
Cristopher <cristophersp706@gmail.com>
2025-04-19 02:42:25 -0400

    V02 Cristopher

---
+++ src/SimulacionDeRestaurante/Models/ResultadoSimulacion.cs
@@ -0,0 +1,13 @@
+
+    public class Resultado
+    {
+        public int NumeroChefs { get; set; }
+        public double TiempoSecuencial { get; set; }
+        public double TiempoParalelo { get; set; }
+        public double Speedup { get; set; }
+        public double Eficiencia { get; set; }
+        public string Recomendacion { get; set; }
+        public int EntregasATiempo { get; set; }
+        public int EntregasGratis { get; set; }
+    }
+
--- src/SimulacionDeRestaurante/obj/Debug/net9.0/SimulacionDeRestaurante.AssemblyInfo.cs
+++ src/SimulacionDeRestaurante/obj/Debug/net9.0/SimulacionDeRestaurante.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimulacionDeRestaurante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f811f0f4565c813d4a06924f2d8c70796180f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6701053e21ef1ef91ff4ec17ced61672bbb44474")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimulacionDeRestaurante")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimulacionDeRestaurante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
