4e0d1cfc5b7fa79d424f52c5b698babb04ad41e8
pame-12 <116987574+pame-12@users.noreply.github.com>
2025-04-18 23:18:35 -0400

    Agregar Clase Pedido
    Representa cada pedido que se realizara en el restaurante.

---
+++ src/SimulacionDeRestaurante/Models/Pedido.cs
@@ -0,0 +1,11 @@
+﻿namespace SimulacionDeRestauranta.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public string Platillo { get; set; }
+        public DateTime HoraPedido { get; set; }
+        public int TiempoEntrega { get; set; }
+        public bool EsGratis { get; set; }
+    }
+}
