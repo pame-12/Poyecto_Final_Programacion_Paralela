8548f6fdf4de12abecfd5f9a2837f45a30e04a73
Ciyei0 <raymondisaac97@gmail.com>
2025-04-23 23:47:42 -0400

    Revert "Main"
    This reverts commit e38b13dcb6b2a2ded39986899d2881a435539c81.
    <Raymond Moreno>

--- src/SimulacionDeRestaurante/Controllers/HomeController.cs
+++ src/SimulacionDeRestaurante/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using SimulacionDeRestauranta.Models;
+using SimulacionDeRestauranta.Services;
 using SimulacionDeRestaurante.Models;
+using System.Diagnostics;
+
 
 namespace SimulacionDeRestaurante.Controllers;
 
@@ -12,16 +15,39 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
-
+    private static string[] menu = { "Yaroa", "Pizza", "Hamburguesa", "Tacos", "Burrito" };
     public IActionResult Index()
     {
+        ViewBag.Menu = menu;
         return View();
     }
+
 
     public IActionResult Privacy()
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult Resultado(SimulacionData data, List<string> menuSeleccionado)
+    {
+        if (ModelState.IsValid)
+        {
+            var cantidadPedidos = data.CantidadPeticiones;
+            _logger.LogInformation($"Cantidad de pedidos: {cantidadPedidos}");
+            _logger.LogInformation($"Menús seleccionados: {string.Join(", ", menuSeleccionado)}");
+
+            var resultadoSimulacion = new RestauranteService().EjecutarSimulacion(cantidadPedidos, menuSeleccionado);
+            return View("Resultado", resultadoSimulacion);
+        }
+        else
+        {
+            ModelState.AddModelError("", "Por favor, ingrese un número válido de pedidos.");
+            ViewBag.Menu = new string[] { "Yaroa", "Pizza", "Hamburguesa", "Tacos", "Burrito" };
+            return View("Index", data);
+        }
+    }
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
